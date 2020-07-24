f62715fd28b778379d7b0f3dbad7dcd50adb1588
Kathy Bui <kathy.bui1992@gmail.com>
2020-07-24 09:27:11 -0600

    Finish user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chef = Chef.create(name: "Kathy")
+
+dish_1 = chef.dishes.create(name: "Pizza", description: "Hawaiian Pizza")
+dish_2 = chef.dishes.create(name: "Pasta", description: "With italian sausage")
+
+ingredient_1 = Ingredient.create(name: "Marinara sauce", calories: 300)
+ingredient_2 = Ingredient.create(name: "Mozzerella", calories: 600)
+ingredient_3 = Ingredient.create(name: "Italian_sausage", calories: 700)
+
+DishIngredient.create(ingredient: ingredient_1, dish: dish_1)
+DishIngredient.create(ingredient: ingredient_2, dish: dish_1)
+DishIngredient.create(ingredient: ingredient_1, dish: dish_2)
+DishIngredient.create(ingredient: ingredient_2, dish: dish_2)
---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "Dish Show Page" do
+  before :each do
+    @chef = Chef.create(name: "Kathy")
+    @dish_1 = @chef.dishes.create(name: "Pizza", description: "Hawaiian Pizza")
+    @ingredient_1 = Ingredient.create(name: "Marinara sauce", calories: 300)
+    @ingredient_2 = Ingredient.create(name: "Mozzerella", calories: 600)
+    DishIngredient.create(ingredient: @ingredient_1, dish: @dish_1)
+    DishIngredient.create(ingredient: @ingredient_2, dish: @dish_1)
+  end
+
+  it "see a list of ingredients for that dish" do
+    visit "/dishes/#{@dish_1.id}"
+    expect(page).to have_content(@dish_1.name)
+    expect(page).to have_content(@ingredient_1.name)
+    expect(page).to have_content(@ingredient_2.name)
+  end
+
+  it "displays the chef's name" do
+    visit "/dishes/#{@dish_1.id}"
+    expect(page).to have_content(@chef.name)
+  end
+end
